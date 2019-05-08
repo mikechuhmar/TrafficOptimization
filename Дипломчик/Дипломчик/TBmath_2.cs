b9b53af0f32b808429b456ee689aac325a8764d2
mikechuhmar <miha1797@gmail.com>
2019-05-08 23:00:09 +0300

    Update TBmath_2.cs

--- Дипломчик/Дипломчик/TBmath_2.cs
+++ Дипломчик/Дипломчик/TBmath_2.cs
@@ -8,16 +8,49 @@ using System.Threading.Tasks;
 namespace Дипломчик
 {
     class TBMath_2
-    {        
+    {
+        public double CIR, Nt, T;
         public TBMath_2()
         {
 
+        }
+        public TBMath_2(double CIR, double Nt, double T)
+        {
+            this.CIR = CIR;
+            this.Nt = Nt;
+            this.T = T;
         }
         public double VTk(double T,double N)
         {
             Random rand = new Random();
             double V = rand.Next(0, Convert.ToInt32(T) / 2);
             return V * N;
+        }
+        public double[] M(double Tk, double RoTk_prev, double V)
+        {
+            double RoTk;
+            double I, GTk, UTk;
+            double[] ch = new double[4];
+
+
+            UTk = CIR * Tk * Nt;
+            RoTk = RoTk_prev + Math.Min(UTk, (T - RoTk_prev));
+            if ((V) <= (RoTk_prev + Math.Min(UTk, (T - RoTk_prev))))
+                I = 1;
+            else I = 0;
+            GTk = V * I;
+            ch[0] = GTk;
+            ch[1] = V;
+            ch[2] = RoTk * Nt;
+
+            RoTk = RoTk - GTk / Nt;
+
+            ch[3] = RoTk;
+
+            
+
+
+            return ch;
         }
         public double[] /*void*/ M(double CIR, double Tk, double T, double Nt, double RoTk_1, double V1)
         {
