27e6cfc72b6dec7d5d0b53de1ebf09f4f1d7cd70
mikechuhmar <miha1797@gmail.com>
2019-05-08 20:16:28 +0300

    Fix

--- Дипломчик/Дипломчик/GeneticAlgorithm.cs
+++ Дипломчик/Дипломчик/GeneticAlgorithm.cs
@@ -8,5 +8,18 @@ namespace Дипломчик
 {
     public class GeneticAlgorithm 
     {
+        private int amChromosomes;
+        private int amGenes;
+        private int amPopulations;
+        private Function func;
+
+        public GeneticAlgorithm(int amChromosomes, int amGenes, int amPopulations, Function func)
+        {
+            this.amChromosomes = amChromosomes;
+            this.amGenes = amGenes;
+            this.amPopulations = amPopulations;
+            this.func = func;
+
+        }
     }
 }
