b4dac7a7ade7dd75412f9a35dd3be1d68416536c
mikechuhmar <miha1797@gmail.com>
2019-05-09 18:03:43 +0300

    Create Data, remove Structures

---
+++ Дипломчик/Дипломчик/Data.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Дипломчик
+{
+    public struct TBStruct
+    {
+        public double CIR, Nt, T;
+        //public double 
+    }
+    public class Data
+    {
+
+
+
+    }
+}
