4e1be7c6489c9c13be8854c63991367b14e80b31
GBMaximilian <GBMaximilian@users.noreply.github.com>
2019-05-26 01:28:50 +0300

    дробление пакета в LB

--- Дипломчик/Дипломчик/Leaky_Bucket_Algoritm.cs
+++ Дипломчик/Дипломчик/Leaky_Bucket_Algoritm.cs
@@ -23,7 +23,7 @@ namespace Дипломчик
                 vs.AddFirst(V1);
             }
             else ch[3] = V1;
-            if (vs.Count != 0)
+            /*if (vs.Count != 0)
             {
                 while ((vs.Count != 0) && (vs.Last.Value < V_Out))
                 {
@@ -31,6 +31,28 @@ namespace Дипломчик
                     Gi_f.AddLast(vs.Last.Value);
                     V_Out = V_Out - vs.Last.Value;
                     vs.RemoveLast();
+                }
+            }
+            */
+            double V_Out_Here = V_Out;
+            if (vs.Count != 0)
+            {
+                while ((vs.Count != 0) && (V_Out_Here > 0))
+                {
+                    if (vs.Last.Value <= V_Out)
+                    {
+                        GTk += vs.Last.Value;
+                        Gi_f.AddLast(vs.Last.Value);
+                        V_Out = V_Out - vs.Last.Value;
+                        vs.RemoveLast();
+                    }
+                    else
+                    {
+                        vs.Last.Value = vs.Last.Value - V_Out;
+                        GTk += V_Out;
+                        Gi_f.AddLast(V_Out);
+                        V_Out_Here = V_Out_Here - V_Out;
+                    }
                 }
             }
 
